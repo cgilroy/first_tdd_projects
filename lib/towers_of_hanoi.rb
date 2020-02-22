f27c3ad99bc08786a06dde74373d8d15cdebd10a
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-02-22 16:40:07 -0700

    Getting some board functionality working

--- lib/towers_of_hanoi.rb
+++ lib/towers_of_hanoi.rb
@@ -1,4 +1,5 @@
 require_relative 'array_methods'
+require 'byebug'
 
 class Game
     def initialize
@@ -14,22 +15,65 @@ class Board
     end
 
     def fill_board
-        disc_size = @columns[0].length
+        disc_size = 1
         @columns[0].each_with_index do |_,index|
-            @columns[0][index] = disc_size
-            disc_size -= 1
+            # @columns[0][index] = Disc.new(disc_size,[0,index])
+            self[[0,index]] = Disc.new(disc_size,[0,index])
+            disc_size += 1
         end
+    end
+
+    def move(from,to)
+        debugger
+        from_item = pickup_disc(from)
+        raise ArgumentError.new "Invalid pickup column" if from_item == nil
+        place_index = place_index(from_item,to)
+        self[place_index] = from_item
+        self[from_item.pos] = nil
+        from_item.pos = place_index
+    end
+
+    def []=(pos,val)
+        @columns[pos[0]][pos[1]] = val
+    end
+
+    def [](pos)
+        @columns[pos[0]][pos[1]]
+    end
+
+    def place_index(move_item,to_column)
+        col = @columns[to_column]
+        place_index = nil
+        col.each_with_index do |item,index|
+            if item.is_a?(Disc)
+                raise ArgumentError.new "Stack is full" if index == 0
+                raise ArgumentError.new "Cannot place disc on larger disc" if item.size < move_item.size
+                return place_index
+            else
+                place_index = [to_column,index]
+            end
+        end
+        place_index
+    end
+
+    def pickup_disc(from_column)
+        col = @columns[from_column]
+        col.each do |item|
+            return item if item.is_a?(Disc)
+        end
+        nil
     end
 
     def render
         transposed_cols = @columns.transpose
+        # debugger
         transposed_cols.each do |print_row|
             print_string = ""
-            print_row.each do |char|
-                if char.nil? then
+            print_row.each do |item|
+                if item.nil? then
                     print_string += "_ "
                 else
-                    print_string = char.to_s + " "
+                    print_string += item.size.to_s + " "
                 end
             end
             puts print_string
@@ -40,7 +84,9 @@ end
 
 class Disc
     attr_reader :size
-    def initialize(size)
+    attr_accessor :pos
+    def initialize(size,pos)
         @size = size
+        @pos = pos
     end
 end
